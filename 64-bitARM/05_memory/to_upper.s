35ca620a58db50eb743d67feb75835d0001583a8
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-09-29 09:54:01 -0700

    fin to_upper

--- 64-bitARM/05_memory/to_upper.s
+++ 64-bitARM/05_memory/to_upper.s
@@ -15,4 +15,30 @@ _start:
 // Loop until byte pointed to at x1 is not 0
 loop:
   ldrb w5, [x4] #1  // load char and incr pointer
-  
+  // if w5 > 'z' -> cont
+  cmp  w5, #'z'
+  b.gt cont
+  // elif < 'a' -> cont
+  cmp  w5, #'a'
+  b.lt cont
+  // else char is lower case [a-z]
+  sub  w5, w5, #('a' - 'A')  // to upper
+
+cont:
+  strb w5, [x3], #1  // store char in w5 to outstr
+  cmp  w5, #0        // reached null terminator? ('\0')
+  b.ne loop          // onto next char
+  // print hex number
+  mov  x0, #1        // 1: stdout
+  ldr  x1, =outstr   // str to print
+  sub  x2, x3, x1    // get len(str) by subrtracting pointers
+  mov  x8, #64       // linux write call
+  svc  0             // execute
+  // exit
+  mov  x0, #0        // 0: return code
+  mov  x8, #93       // 93: terminate
+  svc  0             // execute
+
+.data
+instr:   .asciz "This is the Test String to convert.\n"
+outstr:  .fill 255, 1, 0
