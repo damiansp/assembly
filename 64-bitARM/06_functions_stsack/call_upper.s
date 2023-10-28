bc05603fef05b19d62b5e6288b59784341df67cc
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-10-27 21:33:39 -0700

    init ch 6 with call_upper

---
+++ 64-bitARM/06_functions_stsack/call_upper.s
@@ -0,0 +1,28 @@
+// Convert string to uppper case by calling a function
+// X0-X2: params to linux functions
+// X0:    addr of instr
+// X1:    addr of outstr
+// X8:    linux func no.
+
+.global _start
+
+_start:
+  ldr  x0, =instr   // start of instr
+  ldr  x1, =outstr  // addr of outstr
+  bl   toupper
+
+// print hex number
+  mov  x2, x0       // return code is the length
+  mov  x0, #1       // 1: stdout
+  ldr  x1, =outstr  // str to print
+  mov  x8, #64      // linux write
+  svc  0            // exec
+
+// exit
+  mov  x0, #0       // 0 return code
+  mov  x8, #93      // terminate
+  svc  0            // exex
+
+.data
+instr: .asciz "This is our Test String that we will convert.\n"
+outstr: .fill 255, 1, 0
