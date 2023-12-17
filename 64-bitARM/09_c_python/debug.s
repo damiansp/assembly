4cb16f87ab3b1481b338d70413d205d8ac9a8b7a
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-12-17 15:19:49 -0800

    fin debug macros

--- 64-bitARM/09_c_python/debug.s
+++ 64-bitARM/09_c_python/debug.s
@@ -34,7 +34,37 @@
 
 
 .macro print_str str
-
+  stp  x0, x1, [sp
+  stp  x2, x3, [sp, #-16]!
+  stp  x4, x5, [sp, #-16]!
+  stp  x6, x7, [sp, #-16]!
+  stp  x8, x9, [sp, #-16]!
+  stp  x10, x11, [sp, #-16]!
+  stp  x12, x13, [sp, #-16]!
+  stp  x14, x15, [sp, #-16]!
+  stp  x16, x17, [sp, #-16]!
+  stp  x18, lr, [sp, #-16]!
+  ldr  x0, =1f  // load print str
+  bl   printf   // call printf
+  ldp  x18, lr, [sp], #16
+  ldp  x16, x17, [sp], #16
+  ldp  x14, x15, [sp], #16
+  ldp  x12, x13, [sp], #16
+  ldp  x10, x11, [sp], #16
+  ldp  x8, x9, [sp], #16
+  ldp  x6, x7, [sp], #16
+  ldp  x4, x5, [sp], #16
+  ldp  x2, x3, [sp], #16
+  ldp  x0, x1, [sp], #16
+  b    2f  // branch around str
+1:
+  .asciz "\str\n"
+  .align 4
+2:
 .endm
   
-  
+
+.data
+ptf_str:   .asciz "X%c = %32ld, 0x%016lx\n"
+.align 4
+.text
