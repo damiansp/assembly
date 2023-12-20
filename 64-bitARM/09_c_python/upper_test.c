7e34794ca7c1785e38cda6c7ade4e9151cc6f62f
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-12-20 11:23:35 -0800

    +upper.c

--- 64-bitARM/09_c_python/upper_test.c
+++ 64-bitARM/09_c_python/upper_test.c
@@ -1,1 +1,18 @@
 // C Program to call assembly to_upper routine
+#include <stdio.h>
+extern int mytoupper(char*, char*);
+
+#define MAX_BUFF 255
+
+
+int main() {
+  char* str = "This is a test.";
+  char out_buff[MAX_BUFF];
+  int len;
+
+  len = mytoupper(str, out_buff);
+  printf("Before: %s\n", str);
+  printf("After: %s\n", out_buff);
+  printf("Len: %d\n", len);
+  return 0;
+}
